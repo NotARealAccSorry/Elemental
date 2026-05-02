de7028acf52d8c05a8dd0d156a30fce58914bc0a
NotARealAccSorry <pranavkannan2013@icloud.com>
2026-05-02 10:40:04 -0700

    Restore '4/28/2026, 6:12:32 PM Update README.md'

--- main.ts
+++ main.ts
@@ -1,4 +1,189 @@
+namespace SpriteKind {
+    export const Mouse = SpriteKind.create()
+    export const Card = SpriteKind.create()
+}
+function clickTarget () {
+    sprites.onOverlap(SpriteKind.Mouse, SpriteKind.Enemy, function(sprite: Sprite, otherSprite: Sprite) {
+        pauseUntil(() => browserEvents.MouseLeft.isPressed())
+    })
 
+}
+sprites.onOverlap(SpriteKind.Mouse, SpriteKind.Card, function (sprite, otherSprite) {
+    otherSprite.y = 215
+    otherSprite.z = 5
+    if (browserEvents.MouseLeft.isPressed()) {
+        let discard: Card[] = []
+        easing.easeTo(otherSprite, 160, 120, 2000, easing.Mode.InBack)
+otherSprite.setFlag(SpriteFlag.Ghost, true)
+        pause(2500)
+        targeting = true
+        myTextSprite = fancyText.create("Click A Target")
+        easing.blockEaseTo(otherSprite, 300, 215, 1000, easing.Mode.InOutBack)
+        pause(1350)
+        sprites.destroy(otherSprite)
+        scene.backgroundImage().drawTransparentImage(assets.image`myImage2`, 282, 190)
+        discard.push(hand.removeAt(sprites.readDataNumber(otherSprite, "Index")))
+        drawHand()
+    }
+})
+// Functions
+// CardDrawingFunction
+function doSomething (a: number, b: number, c: number, d: number, e: number, f: number, g: number, i: Image) {
+    textPosition = 27
+    returnImage = image.create(35, 50)
+    returnImage.fillRect(0, 0, 35, 50, f * 2 + 1)
+    returnImage.fillRect(2, 2, 31, 46, 9)
+    returnImage.drawRect(4, 4, 27, 20, 13)
+    returnImage.fillRect(4, 25, 27, 22, 8)
+    returnImage.drawImage(i, 7, 5)
+for (let i = 0; i <= g - 1; i++) {
+        returnImage.drawTransparentImage(assets.image`EC1`, 0, 0 * 8 + 1)
+    }
+    if (a > 0) {
+        fancyText.draw("DMG" + a, returnImage, 5, textPosition, 25, 2, fancyText.tiny_4)
+textPosition += 5
+    }
+    if (b > 0) {
+        fancyText.draw("DFND" + b, returnImage, 5, textPosition, 25, 2, fancyText.tiny_4)
+textPosition += 5
+    }
+    if (c > 0) {
+        fancyText.draw("CSME", returnImage, 5, textPosition, 25, 2, fancyText.tiny_4)
+textPosition += 5
+        returnImage.drawTransparentImage(elementIcons[c - 1], 25, textPosition - 4)
+    }
+    if (d > 0) {
+        fancyText.draw("MTRL", returnImage, 5, textPosition, 25, 2, fancyText.tiny_4)
+textPosition += 5
+        returnImage.drawTransparentImage(elementIcons[d - 1], 25, textPosition - 4)
+    }
+    if (e > 0) {
+        fancyText.draw("APPLY", returnImage, 5, textPosition, 25, 2, fancyText.tiny_4)
+textPosition += 5
+        returnImage.drawTransparentImage(elementIcons[e + 3], 25, textPosition - 4)
+    }
+    return returnImage
+}
+browserEvents.onMouseMove(function (x, y) {
+    if (mouseSprite) {
+        mouseSprite.setPosition(x, y)
+    }
+})
+events.spriteEvent(SpriteKind.Mouse, SpriteKind.Card, events.SpriteEvent.StopOverlapping, function (sprite, otherSprite) {
+    otherSprite.y = 230
+    otherSprite.z = 1
+})
+function drawHand () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Card)
+    index2 =0
+    for (let value of hand) {
+        cardSprite = sprites.create(doSomething(value.damage, value.defense, value.consume, value.materialize, value.enemyEffect, value.rarity, value.cost, value.image), SpriteKind.Card)
+        cardSprite.setPosition(index2 * 25 + 50, 230)
+        sprites.setDataNumber(cardSprite, "Index", index2)
+        index2 += 1
+    }
+}
+let index2 = 0
+let cardSprite: Sprite = null
+let myTextSprite: fancyText.TextSprite = null
+let targeting = false
+let mouseSprite: Sprite = null
+let hand: Card [] = []
+let elementIcons: Image[] = []
+let index = 0
+let textPosition = 0
+let returnImage: Image = null
+let switchingblocks = null
+let list: number[] = []
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 320
+    export const ARCADE_SCREEN_HEIGHT = 240
+}
+class Card {
+    //Properties
+    damage: number
+    defense: number
+    consume: number
+    materialize: number
+    enemyEffect: number
+    rarity: number
+    cost: number
+    image: Image
+    //Constructor
+    constructor(a: number, b: number, c: number, d: number, e: number, f: number, g: number, i: Image) {
+        this.damage = a
+        this.defense = b
+        this.consume = c
+        this.materialize = d
+        this.enemyEffect = e
+        this.rarity = f
+        this.cost = g
+        this.image = i
+    }
+}
+class Enemy {
+    hp: number
+    defense: number
+    enemyEffect: number
+    image: Image
+    constructor (a: number, b: number, c:number, d: Image){
+        this.hp=a
+        this.defense=b
+        this.enemyEffect=c
+        this.image=d
+    }
+}
+// Cards Setup
+let commonCards = [
+new Card(6, 0, 0, 0, 0, 0, 1, assets.image`Chop`),
+new Card(0, 5, 0, 0, 0, 0, 1, assets.image`Defend`),
+new Card(2, 1, 0, 1, 0, 0, 1, assets.image`Inferno`),
+new Card(10, 5, 1, 0, 1, 0, 1, assets.image`Wildfire`),
+new Card(3, 0, 0, 0, 1, 0, 1, assets.image`SlowBurn`),
+new Card(1, 2, 0, 4, 0, 0, 1, img`
+        eeeeeeeeeeeeeeeeeeee
+        eedddeeeddeddddedeee
+        eddddddddddddddedeee
+        edddd9ddddddd9dddeee
+        eed99ddd9dddd99ddeee
+        eeedddddd99dddddeeee
+        eeeee75edddeddddeeee
+        eeeee75e75eee75eeeee
+        eeee75ee75eeee75eeee
+        eeee75eee75eee75eeee
+        eee75eeeee75ee75eeee
+        eee75ee5ee75ee75eeee
+        eeee75eee75eeee75eee
+        eeee75eee75eeee75eee
+        ee5ee75e75ee5e75e5ee
+        eeeee75ee75ee75eeeee
+        e5e77577757e775e7e5e
+        eeee77ee77eee777eeee
+    `),
+new Card(20, 4, 4, 0, 0, 4, 2, img``)
+]
+elementIcons = [
+assets.image`4x4F`,
+assets.image`4x4WA`,
+assets.image`4x4WI`,
+assets.image`4x4L`,
+assets.image`3x3F`,
+assets.image`3x3WA`,
+assets.image`3x3WI`,
+assets.image`3x3L`
+]
+let deck = [
+commonCards[0],
+commonCards[0],
+commonCards[0],
+commonCards[0],
+commonCards[1],
+commonCards[1],
+commonCards[1],
+commonCards[1],
+commonCards[2],
+commonCards[3]
+]
 arrays.shuffle(deck)
 hand = [
 deck.removeAt(0),
@@ -27,5 +212,26 @@ mouseSprite = sprites.create(img`
     `, SpriteKind.Mouse)
 mouseSprite.z = 10
 browserEvents.setCursorVisible(false)
+let myEnemySprite: Sprite
+function readCard(a: Card,b: Enemy) {
+    sprites.changeDataNumberBy(myEnemySprite, "Defense", 0 - a.damage)
 
-
+    if (sprites.readDataNumber(myEnemySprite,"Defense") > 0) {
+        sprites.changeDataNumberBy(myEnemySprite,"Hp",(sprites.readDataNumber(myEnemySprite,"Defense")))
+    }
+   sprites.setDataNumber(myEnemySprite, "enemyEffect", a.enemyEffect)
+}
+function createEnemy(a: Enemy,b: number){
+    let myEnemySprite2 = sprites.create(a.image, SpriteKind.Enemy)
+    sprites.setDataNumber(myEnemySprite2, "Hp", a.hp)    
+    sprites.setDataNumber(myEnemySprite2, "Defense", a.defense)
+    sprites.setDataNumber(myEnemySprite2,"EnemyEffect",a.enemyEffect)
+}
+game.onUpdateInterval(100, function() {
+    if (targeting){
+        clickTarget()
+    }
+    
+})
+createEnemy(new Enemy(50,0,0,assets.image`myImage0`),0)
+drawHand()
